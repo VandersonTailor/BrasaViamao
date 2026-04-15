67756c22c6db1e43c4f00b2d49e262c936a81ec4
vanderson.pinheiro <vanderso10boeira@gmail.com>
2026-04-15 14:18:16 -0300

    chore: upgrade Angular to v21 and refresh configs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent).catch((error: unknown) => {
+bootstrapApplication(AppComponent, {providers: [provideZoneChangeDetection()]}).catch((error: unknown) => {
   console.error(error);
 });
